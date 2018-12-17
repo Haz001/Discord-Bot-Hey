195f180dd418396baeff9f319aa7b8ece54b6001
Haz <24993938+Haz001@users.noreply.github.com>
2018-12-17 00:50:41 +0000

    deleted some things

--- f-data/Meme/add.py
+++
@@ -1,7 +0,0 @@
-while True:
-    address = input("Address:\n>")
-    tag = input("Tag:\n>")
-    file = open("meme.data",'a')
-    file.write(tag+"#"+address+";")
-    file.close()
-    print("Done")
